7bc6fac38f69e3f352febfd68975d85edd99e912
Wayne <fw1036994377@outlook.com>
2019-02-23 16:18:22 +0800

    8.4.2 fork

---
+++ code/ecf/fork.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+int main(){
+	pid_t pid;
+	int x = 1;
+
+	pid = fork();
+	if (pid == 0){
+		printf("child ： x = %d\n",++x);
+		exit(0);
+	}
+
+	printf("parent : x = %d\n", --x);
+	exit(0);
+}
